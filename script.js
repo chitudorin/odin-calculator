f255f34e8eece78d7ef8afe3a4f15c7ef0aaed6a
chitudorin <dorinchitu112@gmail.com>
2022-12-30 21:22:14 +0200

    Add operate function that calls operator functions

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(a, b) {
 function divide(a, b) {
     return Math.round(((+a / +b) + Number.EPSILON) * 1000) / 1000;
 }
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+            break;
+        case "-":
+            return subtract(a, b);
+            break;
+        case "/":
+            return divide(a, b);
+            break;
+        case "*":
+            return multiply(a, b);
+            break;
+        default:
+            return 0;
+    }
+}
